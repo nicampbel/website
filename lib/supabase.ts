1de57f63e35cecf906f1a6264d50380dbdedad81
Nicholas <nicholas.campbell@spatialedge.ai>
2026-03-24 22:58:37 +0200

    Remove service role key

--- lib/supabase.ts
+++ lib/supabase.ts
@@ -6,10 +6,8 @@ import { createClient } from '@supabase/supabase-js';
 const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL!;
 const supabaseAnonKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!;
 
-const supabaseServiceRoleKey = process.env.SUPABASE_SERVICE_ROLE_KEY!;
-
 // Initialize the client
-export const supabase = createClient(supabaseUrl, supabaseServiceRoleKey);
+export const supabase = createClient(supabaseUrl, supabaseAnonKey);
 
 /* 🔥 PRO-TIP: The true power of Supabase + TypeScript is passing a generated 
 Database type into this client. If you do, your code editor will automatically 
